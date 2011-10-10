731dbc1749fca50c7bf2a58f5f1492612ff05e8f
juanibiapina <juanibiapina@gmail.com>
2011-10-10 01:07:00 -0300

    Setup: Fixed permissions for vim files.

--- setup/setup.pp
+++ setup/setup.pp
@@ -5,11 +5,15 @@ package {'vim':
 file {'/home/juanibiapina/.vim':
   ensure => link,
   target => '/home/juanibiapina/development/alias/tools/vim',
+  owner => 'juanibiapina',
+  group => 'juanibiapina',
   require => Package['vim'],
 }
 
 file {'/home/juanibiapina/.vimrc':
   ensure => link,
   target => '.vim/vimrc',
+  owner => 'juanibiapina',
+  group => 'juanibiapina',
   require => Package['vim'],
 }
