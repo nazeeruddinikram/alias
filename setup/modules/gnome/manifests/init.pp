5e0283691d26c39e33181278aba924d5cb4d5de2
juanibiapina <juanibiapina@gmail.com>
2011-11-18 20:55:33 -0200

    Setup: setting 3x3 workspaces.

--- setup/modules/gnome/manifests/init.pp
+++ setup/modules/gnome/manifests/init.pp
@@ -9,4 +9,16 @@ class gnome {
     type => 'list',
     listtype => 'string',
   }
+
+  gconf {'set_horizontal_workspaces':
+    key => '/apps/compiz-1/general/screen0/options/hsize',
+    value => '3',
+    type => 'int',
+  }
+  
+  gconf {'set_vertical_workspaces':
+    key => '/apps/compiz-1/general/screen0/options/vsize',
+    value => '3',
+    type => 'int',
+  }
 }
