0780b34ff5f5ca1d52371e01f98850b610978bce
juanibiapina <juanibiapina@gmail.com>
2011-11-18 11:57:36 -0200

    Setup: fixing the caps key to super. Works now.

--- setup/modules/gnome/manifests/init.pp
+++ setup/modules/gnome/manifests/init.pp
@@ -4,8 +4,6 @@ class gnome {
   }
 
   exec {'make_caps_be_super':
-    command => "/usr/bin/gconftool-2 --type list --list-type string --set /desktop/gnome/peripherals/keyboard/kbd/options '[terminate	terminate:ctrl_alt_bksp,caps	caps:super]'",
-    user => 'juanibiapina',
-    group => 'juanibiapina',
+    command => "/bin/su juanibiapina -c \"/usr/bin/gconftool-2 --config-source='xml::/home/juanibiapina/.gconf' --type list --list-type string --set /desktop/gnome/peripherals/keyboard/kbd/options '[terminate	terminate:ctrl_alt_bksp,caps	caps:super]'\"",
   }
 }
