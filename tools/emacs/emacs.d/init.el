007ae5b8db3fb277beca926cc12ab9c1880285eb
juanibiapina <juanibiapina@gmail.com>
2012-04-02 18:25:04 -0700

    Add emacs auctex.

--- tools/emacs/emacs.d/init.el
+++ tools/emacs/emacs.d/init.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
  '(ido-mode (quote both) nil (ido))
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
