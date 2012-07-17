5d8aeff70e16d7fbb7a03ce003d3c0baa7a7ba5d
juanibiapina <juanibiapina@gmail.com>
2012-07-17 10:59:48 -0300

    Change default gg target.

--- tools/zsh/plugins/gg.sh
+++ tools/zsh/plugins/gg.sh
@@ -1,12 +1,12 @@
 function gg()
 {
     declare -A aliases
-    aliases["gid"]="$HOME/development/gid"
+    aliases["muvs"]="$HOME/development/Muvs"
     aliases["d"]="$HOME/development/"
     aliases["bb"]="$HOME/development/brazil-build/"
     aliases["a"]="$HOME/development/alias/"
     aliases["a2"]="$HOME/development/alias2/"
 
-    cd ${aliases["${1:-gid}"]}
+    cd ${aliases["${1:-muvs}"]}
     pwd
 }
