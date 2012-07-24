3fd2e9535b7c3be9ae30ea089dc3e16df33f558b
juanibiapina <juanibiapina@gmail.com>
2012-07-24 16:22:07 -0300

    Change default gg target to development.

--- tools/zsh/plugins/gg.sh
+++ tools/zsh/plugins/gg.sh
@@ -1,12 +1,11 @@
 function gg()
 {
     declare -A aliases
-    aliases["muvs"]="$HOME/development/Muvs"
     aliases["d"]="$HOME/development/"
     aliases["bb"]="$HOME/development/brazil-build/"
     aliases["a"]="$HOME/development/alias/"
     aliases["a2"]="$HOME/development/alias2/"
 
-    cd ${aliases["${1:-muvs}"]}
+    cd ${aliases["${1:-d}"]}
     pwd
 }
